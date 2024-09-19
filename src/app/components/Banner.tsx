b69770cb93013da9031c9c93081dea600adb7f33
MunimUsmani <112899248+MunimUsmani@users.noreply.github.com>
2024-09-18 23:18:25 -0700

    about done

--- src/app/components/Banner.tsx
+++ src/app/components/Banner.tsx
@@ -1,5 +1,4 @@
 "use client";
-import chatSymbol from "/public/chatsymbol.png";
 
 import React, { useState, useEffect } from "react";
 import Image from "next/image";
@@ -27,16 +26,19 @@ export default function Scroll() {
         {Array(1000)
           .fill("")
           .map((_, index) => (
-            <div key={index} className="flex items-center">
-              <span className="text-[4vw] text-white sm:text-2xl md:text-3xl lg:text-4xl font-light bg-[#5C1616] font-['Clash_Display'] px-4 leading-[40px] sm:leading-[45px] md:leading-[50px] lg:leading-[60px]">
-                Connect And Chat
-                <Image
-                  src="/chatsymbol.png"
-                  alt="Chat Symbol"
-                  width={35}
-                  height={35}
-                />
-              </span>
+            <div key={index} className="flex items-center h-[30%]">
+              <div className="flex items-center  text-[4vw] text-white sm:text-2xl md:text-3xl lg:text-4xl font-light bg-[#5C1616] font-['Clash_Display'] px-4 leading-[40px] sm:leading-[45px] md:leading-[50px] lg:leading-[60px]">
+                <span className="gap-16">Connect And Chat</span>
+                <div className="w-[52] h-[30] gap-2">
+                  <Image
+                    src="/chatsymbol.png"
+                    alt="Chat Symbol"
+                    width={52}
+                    height={70}
+                    className="ml-4 h-[22px]"
+                  />
+                </div>
+              </div>
             </div>
           ))}
       </div>
--- src/app/components/textComponent.tsx
+++ src/app/components/textComponent.tsx
@@ -2,6 +2,8 @@
 import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 export default function TextComponent() {
   useEffect(() => {
@@ -9,24 +11,64 @@ export default function TextComponent() {
       duration: 1000,
       easing: "ease-in-out",
     });
+
+    // Register GSAP's ScrollTrigger
+    gsap.registerPlugin(ScrollTrigger);
+
+    let tl = gsap.timeline();
+
+    // Animation for #heading1
+    tl.to("#heading1", {
+      x: 150,
+      duration: 0.5,
+      scrollTrigger: {
+        trigger: "#heading1",
+        start: "50% center",
+        end: "bottom center",
+        scrub: 2,
+      },
+    });
+
+    // Animation for #heading3
+    tl.to("#heading3", {
+      x: -170,
+      delay: 1,
+      duration: 1,
+      scrollTrigger: {
+        trigger: "#heading3",
+        start: "50% center",
+        end: "bottom center",
+        scrub: 2,
+      },
+    });
   }, []);
 
   return (
-    <div className="bg-[#181815] h-screen flex items-center justify-center">
-      <div className="relative">
+    <div className="bg-[#181815] h-screen flex flex-col sm:flex-col md:flex-row items-center justify-center">
+      {/* Smart */}
+      <div className="relative sm:mb-8 md:mb-0">
         <div className="absolute inset-0 flex items-center justify-center"></div>
-        <div className=""></div>
-        <h1 className="text-7xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-white to-[#999999] font-['Clash_Display'] -mt-32">
+        <h1
+          id="heading1" // Assigning ID for GSAP animation
+          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-white to-[#999999] font-['Clash_Display'] sm:-mt-16 md:-mt-32"
+        >
           Smart
         </h1>
       </div>
+
+      {/* Seamless */}
       <div className="relative z-10 space-y-4">
-        <h1 className="text-7xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-[#D43833] to-[#650300] font-['Clash_Display'] -ml-16">
+        <h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-[#D43833] to-[#650300] font-['Clash_Display'] sm:-ml-8 md:-ml-16">
           Seamless
         </h1>
       </div>
+
+      {/* Scalable */}
       <div>
-        <h1 className="text-7xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-white to-[#999999] font-['Clash_Display'] mt-48 -ml-16">
+        <h1
+          id="heading3" // Assigning ID for GSAP animation
+          className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-medium text-transparent bg-clip-text bg-gradient-to-r from-white to-[#999999] font-['Clash_Display'] sm:mt-24 md:mt-48 sm:-ml-8 md:-ml-16"
+        >
           Scalable
         </h1>
       </div>
